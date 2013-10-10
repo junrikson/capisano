7021b406d9cff4813d3ed982b2cb7687952632df
junrikson <junrikson@gmail.com>
2013-10-10 15:05:03 +0700

    index.php
    Buat folder untuk Web_based

---
+++ web_based/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="Catering Pilih Pesan Online">
+<meta name="keywords" content="Capisano, Catering Online, Catering">
+<meta name="author" content="Capisano">
+<meta name="robots" content="index, follow">
+<title>Halaman Awal</title>
+</head>
+<body>
+<h1>Under Construction</h1>
+</body>
+</html>

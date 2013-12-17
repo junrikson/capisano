fef3bf606ee780f41bcce05a5d1a2f67a60583cd
Yudiramadhan <yura_saintplanet@yahoo.com>
2013-12-18 02:07:34 +0700

    Update FAQ.php

--- web_based/contents/FAQ.php
+++ web_based/contents/FAQ.php
@@ -3,30 +3,36 @@
     <section id="content"><div class="ic"></div>
         <div class="main">
             <div class="container">
-            	<h3>Frequently Asked Questions (Pertanyaan yang sering ditanyakan)</h3>
+            	<h3> (Pertanyaan yang sering ditanyakan)</h3>
                 <ul class="list-1">
                 	<li>
-                    	<a href="#"><strong>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</strong></a>
-                        <span>Sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </span>
-                    </li>
+                    	<a href="#"><strong>Apakah Capisano Murah? </strong></a>
+                        <span>Jika ditanya tentang harga, tentu saja Harga itu relatif tetapi seperti yang di ketahui Semua menu di Catering ini memakai Bahan-bahan dasar yang alami dan segar-segar tentunya. dan diolah sedemikian rupa dengan Arahan dari chef-chef yang berpengalaman dibidangnya tentu saja harga tersebut tidak mahal, dan kami rasa masih terjangkau oleh konsumen-konsumen kami.</span> </li>
                     <li>
-                    	<a href="#"><strong>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet</strong></a>
-                        <span>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.</span>
-                    </li>
+                    	<a href="#"><strong>Apakah Capisano Punya Cabang di Kota Lain? </strong></a>
+                        <span>Kami Tekankan sekali lagi! kami belum mempunyai cabang di tempat lain.jadi apabila ada Catering-Catering yang lain yang mengatasnamakan Capisano kami tidak bertanggung jawab atas tindak tanduk mereka diluar sana. Memang untuk merambah konsumen yang lebih luas dan atas support dari konsumen-konsumen setia kami, maka untuk beberapa bulan kedepan kami telah berencana untuk membuka cabang di beberapa kota besar di Indonesia seperti Jakarta, Bandung Makasar dan Makasar.</span> </li>
                     <li>
-                    	<a href="#"><strong>Nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</strong></a>
-                        <span>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.</span>
-                    </li>
-                    <li>
-                    	<a href="#"><strong>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</strong></a>
-                        <span>Vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.</span>
-                    </li>
-                    <li>
-                    	<a href="#"><strong>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</strong></a>
-                        <span>Et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </span>
-                    </li>
+                    	<a href="#"><strong>Siapakah yang terlibat dalam membuat masakan ini? </strong></a><span>Banyak sekali yang menanyakan pertanyaan ini. jadi kami sampaikan bahwa banyak Chef-Chef yang sudah malang-melintang dalam dunia  menciptakan menu-menu yang berkelas.Berikut beberapa Chef-Chef yang terlibat dalam Bisnis Catering ini antara lain;</span></li>
                 </ul>
-            </div>
+				
+				
+				<figure><a href="./contents/Chef1.php"><img src="images/rocco.jpg" alt=""></a></figure>
+				<a href="./contents/Chef1.php">Profile Selengkapnya </a>
+				
+				<figure><a href="./contents/Chef2.php"><img src="images/Edwin.jpg" alt=""></a></figure>
+				<a href="./contents/Chef2.php">Profile Selengkapnya </a>
+				
+				<figure><a href="./contents/Chef3.php"><img src="images/Boby.jpg" alt=""></a></figure>
+				<a href="./contents/Chef3.php">Profile Selengkapnya </a>
+                
+				
+				
+                <ul class="list-1">
+                  <li>
+                    <a href="#"><strong>Beberapa menyakinkankah Capisano ini?</strong></a>
+                    <span>Capisano telah melakukan beberapa kerjasama Oleh beberapa Instansi seperti;Bank BCA,BNI,BII,Mandiri dan beberapa bank local lainnya. Kami juga telah menjalin kerjasama di beberapa Instansi baik Pemerintah maupun Swasta seperti; PT.London Sumatera, PT, PTPN$ dan lain-lain.</span>                    </li>
+                </ul>
+          </div>
         </div>
     </section>
     

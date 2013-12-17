9fc51ddb2c664afb443a53ee1c145fb65aee167d
Yudiramadhan <yura_saintplanet@yahoo.com>
2013-12-18 02:32:03 +0700

    Update main.php

--- web_based/contents/main.php
+++ web_based/contents/main.php
@@ -6,17 +6,26 @@
                 <article class="col-1">
                 	<a href="#"><img class="img-border" src="images/banner-1.jpg" alt=""></a>
                 </article>
-                <article class="col-1">
+                <article class="col-2">
                 	<a href="#"><img class="img-border" src="images/banner-2.jpg" alt=""></a>
                 </article>
-                <article class="col-2">
+                <article class="col-3">
                 	<a href="#"><img class="img-border" src="images/banner-3.jpg" alt=""></a>
+            </div>
+						<article class="col-1">
+                	<a href="#"><img class="img-border" src="images/banner-4.jpg" alt=""></a>
                 </article>
+                <article class="col-2">
+                	<a href="#"><img class="img-border" src="images/banner-5.jpg" alt=""></a>
+                </article>
+                <article class="col-3">
+                	<a href="#"><img class="img-border" src="images/banner-6.jpg" alt=""></a>
             </div>
             <div class="wrapper">
-            	<article class="column-1">
-                	<div class="indent-left">
+            	<article class="column-2">
+                	<div class="indent-right">
 						<div class="maxheight indent-bot">
+                
                         
                         <?php
 						if(!cek()){
@@ -42,11 +51,11 @@
                 <article class="column-2">
                 	<div class="maxheight indent-bot">
                         <h3 class="p1">About the Catering</h3>
-                        <h6 class="p2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h6>
-                        <p class="p2">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                        Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. 
-                    </div>
-                    <a class="button-2" href="#">Read More</a>
+                        <h6 class="p2">Catering ini adalah Catering terbaik yang menyajikan sajian-sajian  yang akan memanjakan Lidah anda setaip harinya. Semua masakan yang di sediakan di Catering kami dibuat oleh Chef-Chef berpengalaman..</h6>
+                        <p class="p2">Visi Misi Kami adalah menyajikan masakan-masakan Internasional dengan Harga yang bersahabat sehingga dapat dinikmati setiap harinya tampa harus menguras dompet dari pelanggan pelanggan setia kami.<cite>Tiap Rasa yang anda Rasakan adalah tanggung jawab kami yang memuaskannya.</cite></p>
+                       <p class="p2"> Alamat dari Usaha Catering ini dapat anda kunjungi pada pada Jalan.Thamrin No.432 (Sebelah bang BII cabang Thamrin) atau telp: 061-7896767 ,061-7658989 Fax:061-7942178 Medan,Sumatera Utara</p>
+					   <h3> Lihat Komentar Para Pelanggan Setia Kami</h3>
+                    <a class="button-2" href="https://twitter.com/CapisanoNo1">Twitter</a>
                 </article>
             </div>
         </div>
